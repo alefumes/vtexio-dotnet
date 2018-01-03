013e3b0615c4f46096ba6f548875436ff50cd389
Rafael Bacelar <rafael.bacelar@vtex.com.br>
2018-01-03 17:54:47 -0200

    Initial commit.

---
+++ service/Controllers/RoutesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace service.Controllers
+{
+    public class RoutesController : Controller
+    {
+        public string PrintHelloWorld()
+        {
+            return "Hello, IO!";
+        } 
+
+        public string PrintHello(string name)
+        {
+            return $"Hello, {name}!";
+        } 
+      
+        public string PrintPrivateValue(string account, string workspace) 
+        {
+            return $"My private value! Account: {account} | Workspace: {workspace}";
+        }
+    }
+}
